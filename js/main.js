8c8e3f57644929f75373ed535448aa96586c4d5d
paul0127 <paul@eso.com.tw>
2021-09-22 17:48:27 +0800

    index

--- js/main.js
+++
@@ -1,6 +0,0 @@
-let nav_btn = document.querySelector('.left_menu .nav_btn')
-
-nav_btn.addEventListener('click',()=>{
-  nav_btn.parentElement.classList.toggle('active')
-  document.body.classList.toggle('fixed')
-})
